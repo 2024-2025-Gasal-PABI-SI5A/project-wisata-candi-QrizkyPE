88cfeb1f3ee261eecd2e96ccb3eb2834324c346f
Qrizky <qpeffendy@gmail.com>
2024-11-15 17:38:31 +0700

    Part 2,3,4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/signin_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -11,17 +14,27 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Candi',
-      theme:  ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      useMaterial3: true,
-      ),
-      home : DetailScreen(
+      title: 'wisata candi',
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.blueAccent),
+            titleTextStyle: TextStyle(
+              color: Color.fromARGB(255, 56, 112, 185),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.blueAccent).copyWith(
+            primary: Colors.blueAccent,
+            surface: Colors.blueGrey[50],
+          ),
+          useMaterial3: true),
+      home: DetailScreen(
         candi: candiList[0],
       ),
-
-      );
+      // home: ProfileScreen(),
+      // home: SignInScreen(),
+    );
   }
 }
-
-
-      
